61fa6ff92b39e8d7a325deeac1f9d0cb51875c9f
Joeispro99 <65899074+Joeispro99@users.noreply.github.com>
2020-11-26 16:26:03 +0800

    Pie Chart Complete *DO NOT TOUCH MARKED SECTION*

---
+++ ฿u₫g€+/฿u₫g€+/NSUIColor+Hex.swift
@@ -0,0 +1,26 @@
+//
+//  NSUIColor+Hex.swift
+//  ฿u₫g€+
+//
+//  Created by Joe Wong on 26/11/20.
+//
+
+import Foundation
+import Charts
+//MARK: DO NOT TOUCH EXTENSION
+extension NSUIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red)/255.0, green: CGFloat(green)/255.0, blue: CGFloat(blue)/255.0, alpha: 1.0)
+    }
+    convenience init(hex: Int) {
+        self.init(
+            red: (hex >> 16) & 0xFF,
+            green: (hex >> 8) & 0xFF,
+            blue: hex & 0xFF
+        )
+    }
+}
--- ฿u₫g€+/฿u₫g€+/ViewController.swift
+++ ฿u₫g€+/฿u₫g€+/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pieView: PieChartView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupPieChart()
     }
-
-
+    //MARK: DO NOT TOUCH FUNCTION
+    func setupPieChart() {
+        pieView.chartDescription?.enabled = false
+        pieView.drawHoleEnabled = false
+        pieView.rotationAngle = 0
+        pieView.rotationEnabled = true
+        pieView.isUserInteractionEnabled = true
+        /*
+        pieView.legend.enabled = false
+ */
+        pieView.legend.horizontalAlignment = .center
+        pieView.backgroundColor = .systemBackground
+        var entries: [PieChartDataEntry] = Array()
+        entries.append(PieChartDataEntry(value: 50.0, label: "Healthcare"))
+        entries.append(PieChartDataEntry(value: 40.0, label: "Transport"))
+        entries.append(PieChartDataEntry(value: 30.0, label: "Food"))
+        entries.append(PieChartDataEntry(value: 20.0, label: "Gaming"))
+        entries.append(PieChartDataEntry(value: 10.0, label: "Entertainment"))
+        
+        let dataSet = PieChartDataSet(entries: entries, label: "")
+        
+        let c1 = NSUIColor(hex: 0x3A015C)
+        let c2 = NSUIColor(hex: 0x4F0147)
+        let c3 = NSUIColor(hex: 0x35012C)
+        let c4 = NSUIColor(hex: 0x290025)
+        let c5 = NSUIColor(hex: 0x11001C)
+        
+        dataSet.colors = [c1, c2, c3, c4, c5]
+        dataSet.drawValuesEnabled = false
+        
+        pieView.data = PieChartData(dataSet: dataSet)
+    }
 }
 
