8524f85d3407020619fbc27662a85e5f33eb5aea
granwyntan <granwyn_tan@s2019.ssts.edu.sg>
2020-11-27 11:24:25 +0800

    Pie Chart Improvements

--- ฿u₫g€+/฿u₫g€+/ViewController.swift
+++ ฿u₫g€+/฿u₫g€+/ViewController.swift
@@ -8,45 +8,64 @@
 import UIKit
 import Charts
 
+extension Date {
+    func monthAsString() -> String {
+            let df = DateFormatter()
+            df.setLocalizedDateFormatFromTemplate("MMMM")
+            return df.string(from: self)
+    }
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var pieView: PieChartView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         setupPieChart()
     }
+    
     //MARK: DO NOT TOUCH FUNCTION
     func setupPieChart() {
         pieView.chartDescription?.enabled = false
-        pieView.drawHoleEnabled = false
+        pieView.drawHoleEnabled = true
         pieView.rotationAngle = 0
         pieView.rotationEnabled = true
+        pieView.centerText = Date().monthAsString()
+        pieView.drawSlicesUnderHoleEnabled = true
+        pieView.drawCenterTextEnabled = true
         pieView.isUserInteractionEnabled = true
         /*
         pieView.legend.enabled = false
  */
         pieView.legend.horizontalAlignment = .center
+        pieView.legend.textColor = .label
+        pieView.legend.wordWrapEnabled = true
         pieView.backgroundColor = .systemBackground
         var entries: [PieChartDataEntry] = Array()
-        entries.append(PieChartDataEntry(value: 50.0, label: "Healthcare"))
-        entries.append(PieChartDataEntry(value: 40.0, label: "Transport"))
-        entries.append(PieChartDataEntry(value: 30.0, label: "Food"))
-        entries.append(PieChartDataEntry(value: 20.0, label: "Gaming"))
-        entries.append(PieChartDataEntry(value: 10.0, label: "Entertainment"))
-        
+        entries.append(PieChartDataEntry(value: 600.00, label: "Healthcare"))
+        entries.append(PieChartDataEntry(value: 500.00, label: "Transport"))
+        entries.append(PieChartDataEntry(value: 400.00, label: "Food"))
+        entries.append(PieChartDataEntry(value: 300.00, label: "Gaming"))
+        entries.append(PieChartDataEntry(value: 200.00, label: "Entertainment"))
+        entries.append(PieChartDataEntry(value: 100.00, label: "Others"))
         let dataSet = PieChartDataSet(entries: entries, label: "")
         
-        let c1 = NSUIColor(hex: 0x3A015C)
-        let c2 = NSUIColor(hex: 0x4F0147)
-        let c3 = NSUIColor(hex: 0x35012C)
-        let c4 = NSUIColor(hex: 0x290025)
-        let c5 = NSUIColor(hex: 0x11001C)
+//        let c1 = NSUIColor(hex: 0x3A015C)
+//        let c2 = NSUIColor(hex: 0x4F0147)
+//        let c3 = NSUIColor(hex: 0x35012C)
+//        let c4 = NSUIColor(hex: 0x290025)
+//        let c5 = NSUIColor(hex: 0x11001C)
         
-        dataSet.colors = [c1, c2, c3, c4, c5]
-        dataSet.drawValuesEnabled = false
-        
+//        dataSet.colors = [.systemRed, .systemOrange, .systemYellow, .systemGreen, .systemTeal, .systemBlue,  .systemIndigo, .systemPurple]
+        dataSet.colors =
+           ChartColorTemplates.material()
+                + ChartColorTemplates.joyful()
+                + ChartColorTemplates.colorful()
+                + ChartColorTemplates.pastel()
+        dataSet.drawValuesEnabled = true
         pieView.data = PieChartData(dataSet: dataSet)
     }
 }
