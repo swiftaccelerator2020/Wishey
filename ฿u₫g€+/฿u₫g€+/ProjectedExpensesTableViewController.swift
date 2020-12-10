497e3afc68c14678b6d7db5737ea5de34ff39177
adam061606 <62824117+adam061606@users.noreply.github.com>
2020-12-10 10:40:51 +0800

    Update ProjectedExpensesTableViewController.swift

--- ฿u₫g€+/฿u₫g€+/ProjectedExpensesTableViewController.swift
+++ ฿u₫g€+/฿u₫g€+/ProjectedExpensesTableViewController.swift
@@ -9,10 +9,6 @@ import UIKit
 
 class ProjectedExpensesTableViewController: UITableViewController {
     
-    var projectedIncomeArray = [projectedIncome(incomeName: "Salary",incomeMoney: 3400), projectedIncome(incomeName: "Other",incomeMoney: 6), projectedIncome(incomeName: "Savings",incomeMoney: 3324)]
-    
-    var projectedExpensesArray = [projectedExpenses(expenseName: "Food",expenseMoney: 30),projectedExpenses(expenseName: "Transport",expenseMoney: 20),projectedExpenses(expenseName: "Healthcare",expenseMoney: 20),projectedExpenses(expenseName: "Entertainment",expenseMoney: 12)]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
