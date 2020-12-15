ee6451435680e4a930d272705e0821b100e685a9
adam061606 <62824117+adam061606@users.noreply.github.com>
2020-12-16 00:01:51 +0800

    wait hum for some reason idk why text got error sry but too bad its midnight alr so hum seabass u can pull this although there isn't much change. also joe for some reason was uncontactable for the last 12h so.....

--- ฿u₫g€+/฿u₫g€+/expenseDetailTableViewController.swift
+++ ฿u₫g€+/฿u₫g€+/expenseDetailTableViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 class expenseDetailTableViewController: UITableViewController {
 
     @IBOutlet weak var expenseNameTF: UITextField!
-    @IBOutlet weak var amountSpentTF: UITableViewCell!
+    @IBOutlet weak var amountSpentTF: UITextField!
     @IBOutlet weak var projectedExpensesTF: UITextField!
-    
+    var expense: expenseStruct!
+    var theIndexPath: IndexPath!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        projectedExpensesTF.keyboardType = .numberPad
+        amountSpentTF.keyboardType = .decimalPad
+//        amountSpentTF.text = String(format: "%.2f", EXPENSES.actualExpenses)
+        expenseNameTF.layer.cornerRadius = 5
+        expenseNameTF.clipsToBounds = true
+        amountSpentTF.layer.cornerRadius = 5
+        amountSpentTF.clipsToBounds = true
+        projectedExpensesTF.layer.cornerRadius = 5
+        projectedExpensesTF.clipsToBounds = true    
+        expenseNameTF.text = expense.categoryName
+        amountSpentTF.text = String(expense.actualExpenses)
+        projectedExpensesTF.text = String(expense.projectedExpenses)
+        
+        self.hideKeyboardWhenTappedAround()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -24,7 +40,31 @@ class expenseDetailTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField == amountSpentTF {
+            guard let oldText = textField.text, let r = Range(range, in: oldText) else {
+                return true
+            }
+            
+            let newText = oldText.replacingCharacters(in: r, with: string)
+            let isNumeric = newText.isEmpty || (Double(newText) != nil)
+            let numberOfDots = newText.components(separatedBy: ".").count - 1
+            
+            let numberOfDecimalDigits: Int
+            if let dotIndex = newText.firstIndex(of: ".") {
+                numberOfDecimalDigits = newText.distance(from: dotIndex, to: newText.endIndex) - 1
+            } else {
+                numberOfDecimalDigits = 0
+            }
+            
+            return isNumeric && numberOfDots <= 1 && numberOfDecimalDigits <= 2
+        }
+        let invalidCharacters =
+          CharacterSet(charactersIn: "0123456789").inverted
+        return (string.rangeOfCharacter(from: invalidCharacters) == nil)
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
@@ -34,8 +74,7 @@ class expenseDetailTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
@@ -43,8 +82,36 @@ class expenseDetailTableViewController: UITableViewController {
 
         return cell
     }
-    */
-
+    
+    @IBAction func dismissKeyboard(_ sender: UITextField) {
+        sender.resignFirstResponder()
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+//        dismiss(animated: true, completion: nil)
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        if expenseNameTF.text != nil && amountSpentTF.text != nil && projectedExpensesTF.text != nil {
+            if Double(amountSpentTF.text!) != nil && Int(projectedExpensesTF.text!) != nil {
+                expensesArray[theIndexPath.row]  = expenseStruct(categoryName: expenseNameTF.text!, projectedExpenses: Int(Double(amountSpentTF.text!)!), actualExpenses: Double(Int(projectedExpensesTF.text!)!))
+//                dismiss(animated: true, completion: nil)
+                print(expensesArray)
+                performSegue(withIdentifier: "unwindSegue", sender: nil)
+            } else {
+//                print("cost or duration is not a number/int")
+                let alert = UIAlertController(title: "Warning", message: "Cost or duration is not a whole number", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        } else {
+            let alert = UIAlertController(title: "Warning", message: "Not all required text fields are filled up", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
