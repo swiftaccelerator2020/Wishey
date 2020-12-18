53ebb48678135d275316954d2c3213d9274c477d
granwyntan <granwyn_tan@s2019.ssts.edu.sg>
2020-12-18 20:12:00 +0800

    Idk how name commits

--- ฿u₫g€+/฿u₫g€+/expensesTableViewController.swift
+++ ฿u₫g€+/฿u₫g€+/expensesTableViewController.swift
@@ -18,7 +18,7 @@ class expensesTableViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
         
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+         self.navigationItem.leftBarButtonItem = self.editButtonItem
     }
     
     // MARK: - Table view data source
